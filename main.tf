dc982b62d638b30ab62632f01e4a0fe6fd681c3d
Nicolas Da Mutten <nicolas.damutten@contactify.ch>
2023-12-22 16:49:08 +0100

    chore: update and pin terraform to version that is supposed to fix it

--- main.tf
+++ main.tf
@@ -6,6 +6,10 @@ terraform {
     key                  = "terraform.tfstate"
   }
   required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.85.0"
+    }
     azuread = {
       source  = "hashicorp/azuread"
       version = "2.43.0"
